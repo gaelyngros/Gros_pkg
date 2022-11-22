236393477c6cc9f11043569b42de139cf6c036c2
gaelyngros <gaelyn.gros@selu.edu>
2022-11-22 21:35:33 +0000

    updated r functions for grosproj package

--- R/data_clean.R
+++ R/data_clean.R
@@ -8,10 +8,11 @@
 #' @examples
 #' clean_data("data/ac_field_data.csv")
 #' 
+#' 
 clean_data <- function(filepath) {
   data_raw <- read_csv(filepath)
   data_clean <- data_raw %>%
     drop_na()
-  write.csv(file = "/cloud/project/data/ac_clean.csv", data_clean)
+  write.csv(file = "ac_clean.csv", data_clean)
   return(is.na(data_clean))
 }
